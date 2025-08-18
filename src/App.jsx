ee094dbdd7cda4fa9aea929afdc1734188b86053
Ayush <ayushchoudhary082003@gmail.com>
2025-08-18 14:57:09 +0530

    create header components

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignOut from "./pages/SignOut";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md ">
+      <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Ayush</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+
+        <form className="bg-slate-100 p-3 rounded-lg shadow-md flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none  w-24 sm:w-64 md:w-96 lg:w-96 xl:w-96 text-slate-700"
+          />
+          <FaSearch className="text-slate-700 cursor-pointer" />
+        </form>
+        <ul className="flex gap-4 ">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/signin">
+            <li className=" sm:inline text-slate-700 hover:underline">
+              Sign in
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
