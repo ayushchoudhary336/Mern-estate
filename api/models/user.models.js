5861670d124c5f00ed7ab9af50f59e1a7abac832
Ayush <ayushchoudhary082003@gmail.com>
2025-08-19 02:21:47 +0530

    create user models

---
+++ api/models/user.models.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true, // Automatically manage createdAt and updatedAt fields
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
